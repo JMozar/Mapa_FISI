c30c45d22d3559c5c51b76359e99cf632f72dc83
JMozar <jhon.moore@unmsm.edu.pe>
2023-10-11 14:57:31 -0500

    Curso

--- app/vista/frm_cursos.php
+++ app/vista/frm_cursos.php
@@ -95,7 +95,7 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" style="position: relative;left: 25%; font-size: 28px;">DATOS DEL CUSO</h5>
+        <h5 class="modal-title" style="position: relative;left: 25%; font-size: 28px;">DATOS DEL CURSO</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close" onclick="cerrarModal()">
           <span aria-hidden="true">&times;</span>
         </button>
